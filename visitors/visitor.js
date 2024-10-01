741af4e99d34f27c50a8b747e091676611d579d4
bj9322 <bj932218@gmail.com>
2024-10-01 17:53:27 +0900

    test

---
+++ visitors/visitor.js
@@ -0,0 +1,17 @@
+// 댓글 작성란: 댓글(텍스트), 사진(이미지 추가 가능?)
+// 등록 버튼을 누르면 다른 div에 추가되는 형식
+
+
+const userInput = document.getElementById('user-input');
+const addBtn = document.getElementById('addBtn');
+const container = document.getElementById('container');
+
+
+addBtn.addEventListener('click', function() {
+    const text = userInput.value; 
+    const paragraph = document.createElement('p'); 
+    paragraph.textContent = text; 
+    container.appendChild(paragraph); 
+    userInput.value = ''; 
+});
+
