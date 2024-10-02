ae7ffd530723d552d70e4ad2e86a94fdbe5b29ee
kangdaeeun <daeeun123@daum.net>
2024-10-02 14:48:53 +0900

    모달연습2

---
+++ test/script.js
@@ -0,0 +1,34 @@
+const member1 = document.getElementById('member1');
+const member2 = document.getElementById('member2');
+const member3 = document.getElementById('member3');
+const member4 = document.getElementById('member4');
+const member5 = document.getElementById('member5');
+const modalCloseButton = document.getElementById('modalCloseButton');
+const modal = document.getElementById('modalContainer');
+
+member1.addEventListener('click', () => {
+    $('#modalContainer').toggle();
+});
+
+member2.addEventListener('click', () => {
+    $('#modalContainer').toggle();
+});
+
+member3.addEventListener('click', () => {
+    $('#modalContainer').toggle();
+});
+
+member4.addEventListener('click', () => {
+    $('#modalContainer').toggle();
+});
+
+member5.addEventListener('click', () => {
+    $('#modalContainer').toggle();
+});
+
+modalCloseButton.addEventListener('click', () => {
+    $('#modalContainer').toggle();
+});
+
+// document.querySelector('')
+
