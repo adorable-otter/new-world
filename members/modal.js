3e5f231a20c00a471477a4c201a8e12b0753bb07
kangdaeeun <daeeun123@daum.net>
2024-10-04 15:34:45 +0900

    모달 정의 빼먹음

--- members/modal.js
+++ members/modal.js
@@ -1,22 +1,18 @@
-// const member1 = document.getElementById('member1');
-// const member2 = document.getElementById('member2');
-// const member3 = document.getElementById('member3');
-// const member4 = document.getElementById('member4');
-// const member5 = document.getElementById('member5');
-// const modalCloseButton1 = document.getElementById('modalCloseButton1');
-// const modalCloseButton2 = document.getElementById('modalCloseButton2');
-// const modalCloseButton3 = document.getElementById('modalCloseButton3');
-// const modalCloseButton4 = document.getElementById('modalCloseButton4');
-// const modalCloseButton5 = document.getElementById('modalCloseButton5');
-// const modal1 = document.getElementById('modalContainer1');
-// const modal2 = document.getElementById('modalContainer2');
-// const modal3 = document.getElementById('modalContainer3');
-// const modal4 = document.getElementById('modalContainer4');
-// const modal5 = document.getElementById('modalContainer5');
-
-function openclose() {                       // 이게 위의 노가다를 깔삼하게 정리함
-    $('#').toggle();
-}
+const member1 = document.getElementById('member1');
+const member2 = document.getElementById('member2');
+const member3 = document.getElementById('member3');
+const member4 = document.getElementById('member4');
+const member5 = document.getElementById('member5');
+const modalCloseButton1 = document.getElementById('modalCloseButton1');
+const modalCloseButton2 = document.getElementById('modalCloseButton2');
+const modalCloseButton3 = document.getElementById('modalCloseButton3');
+const modalCloseButton4 = document.getElementById('modalCloseButton4');
+const modalCloseButton5 = document.getElementById('modalCloseButton5');
+const modal1 = document.getElementById('modalContainer1');
+const modal2 = document.getElementById('modalContainer2');
+const modal3 = document.getElementById('modalContainer3');
+const modal4 = document.getElementById('modalContainer4');
+const modal5 = document.getElementById('modalContainer5');
 
 member1.addEventListener('click', () => {
     $('#modalContainer1').toggle();
